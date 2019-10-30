f579de818114ec6ed6877aac9d9a318bf1d571fa
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-10-30 18:53:31 +0300

    Implement the ButtonPanel component

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // eslint-disable-next-line class-methods-use-this
   render() {
     return (
-      <div id="App">
+      <div>
         <Display />
         <ButtonPanel />
       </div>
