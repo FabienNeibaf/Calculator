292883ce16f0b25bc2cc276e89c0c96fd8a6a863
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-10-30 20:44:35 +0300

    Pass color and wide props to the Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <button className={name === '0' ? 'zero' : ''}>{name}</button>
+const Button = ({ name, color = '#db8619', wide = false }) => (
+  <button style={{ backgroundColor: color }} className={wide ? 'x2' : ''}>
+    {name}
+  </button>
 );
 
 Button.propTypes = {
   name: PropTypes.string,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,32 +5,32 @@ const ButtonPanel = () => {
   return (
     <section id="ButtonPanel">
       <div className="group">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name={`\u0025`} />
+        <Button name="AC" color="#eee" />
+        <Button name="+/-" color="#eee" />
+        <Button name={`\u0025`} color="#eee" />
         <Button name={`\u00f7`} />
       </div>
       <div className="group">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
+        <Button name="7" color="#eee" />
+        <Button name="8" color="#eee" />
+        <Button name="9" color="#eee" />
         <Button name="X" />
       </div>
       <div className="group">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
+        <Button name="4" color="#eee" />
+        <Button name="5" color="#eee" />
+        <Button name="6" color="#eee" />
         <Button name="-" />
       </div>
       <div className="group">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
+        <Button name="1" color="#eee" />
+        <Button name="2" color="#eee" />
+        <Button name="3" color="#eee" />
         <Button name="+" />
       </div>
       <div className="group">
-        <Button name="0" />
-        <Button name="." />
+        <Button name="0" color="#eee" wide={true} />
+        <Button name="." color="#eee" />
         <Button name="=" />
       </div>
     </section>
