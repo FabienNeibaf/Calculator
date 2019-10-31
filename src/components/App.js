42b751e89c122d592bb26619646b9f9c95f4f73b
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-10-31 13:36:29 +0300

    App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      next: null,
+      total: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  // eslint-disable-next-line class-methods-use-this
+  handleClick(buttonName) {
+    this.setState(prevState => calculate(prevState, buttonName));
+  }
+
   render() {
+    const { total, handleClick } = this.state;
     return (
       <div id="App">
-        <Display />
-        <ButtonPanel />
+        <Display result={total} />
+        <ButtonPanel clickHandler={handleClick} />
       </div>
     );
   }
