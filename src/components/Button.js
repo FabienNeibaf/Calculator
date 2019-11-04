49af62dc4afcae8dfb1d29f72c59628c7324d87a
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-11-04 14:33:17 +0300

    Minor fix

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name, color = '#db8619', wide = false }) => (
+const Button = ({ name, color, wide }) => (
   <button style={{ backgroundColor: color }} className={wide ? 'x2' : ''}>
     {name}
   </button>
 );
+
+Button.defaultProps = {
+  wide: false,
+  color: '#db8619',
+};
 
 Button.propTypes = {
   name: PropTypes.string,
